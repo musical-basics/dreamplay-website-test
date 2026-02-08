6925e2ce75773f79d920526c3eeb3c5656c00672
Lionel Yu <support@musicalbasics.com>
2026-02-07 18:32:22 -0800

    Redirect /checkout-pages/customize to /customize

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/checkout-pages/customize',
+        destination: '/customize',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
